b7ccaed4fca3c2e904e2ca48cfcf13c59a6e4eff
lisnop <zhaolei@gmail.com>
2015-06-08 23:28:20 +0800

    bug fix

--- db.config.php
+++ db.config.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'rb.php';
 {
-$db_host=getenv('MYSQL_PORT_3306_TCP');
+$db_host=getenv('MYSQL_PORT_3306_TCP_ADDR');
 $db_name=getenv('MYSQL_INSTANCE_NAME');
 $db_uname=getenv('MYSQL_USERNAME');
 $db_upass=getenv('MYSQL_PASSWORD');
