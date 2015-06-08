af75ef600c431c52f8c5acdf45dd4f6c3680e7f5
lisnop <zhaolei@gmail.com>
2015-06-08 22:58:17 +0800

    revision 20150608

--- db.config.php
+++ db.config.php
@@ -1,4 +1,10 @@
 <?php
 require_once 'rb.php';
-R::setup('mysql:host=127.0.0.1;dbname=zerohf','zerohf','zerohf2015');
+{
+$db_host=getenv('MYSQL_PORT_3306_TCP');
+$db_name=getenv('MYSQL_INSTANCE_NAME');
+$db_uname=getenv('MYSQL_USERNAME');
+$db_upass=getenv('MYSQL_PASSWORD');
+R::setup('mysql:host='.$db_host.';dbname='.$db_name,$db_uname,$db_upass);
+}
 //R::freeze( TRUE );
