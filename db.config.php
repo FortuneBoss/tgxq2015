f1112f00802da80459552b6bf0be5a8adb8eab74
lisnop <zhaolei@gmail.com>
2015-06-08 23:47:14 +0800

    debug

--- db.config.php
+++ db.config.php
@@ -2,13 +2,17 @@
 require_once 'rb.php';
 {
 $db_host=getenv('MYSQL_PORT_3306_TCP_ADDR');
+$db_port=getenv('MYSQL_PORT_3306_TCP_PORT');
 $db_name=getenv('MYSQL_INSTANCE_NAME');
 $db_uname=getenv('MYSQL_USERNAME');
 $db_upass=getenv('MYSQL_PASSWORD');
 var_dump($db_host);
+var_dump($db_port);
 var_dump($db_name);
 var_dump($db_uname);
 var_dump($db_upass);
-R::setup('mysql:host='.$db_host.';dbname='.$db_name,$db_uname,$db_upass);
+$dsn='mysql:host='.$db_host.';port='.$db_port.';dbname='.$db_name;
+var_dump($dsn);
+R::setup($dsn,$db_uname,$db_upass);
 }
 //R::freeze( TRUE );
